0e40c02350b0a10768a1fca45d7d342fabd54f15
Mathieu Le Puil <lepuilmathieu@gmail.com>
2024-03-18 10:44:38 +0100

    feat: create form

---
+++ src/Form/ChatbotType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChatbotType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('question', TextType::class)
+            ->add('submit', SubmitType::class, ['label' => 'Ask the chatbot'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
